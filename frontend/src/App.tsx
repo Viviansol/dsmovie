65f9a65532e1836b3c166166f2f20c0f66736a61
Viviansol <viviansan93@gmail.com>
2022-03-09 18:17:35 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
-
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 function App() {
   return (
-    <h1>Pagina principal</h1>
+    <header>
+      <nav>
+        <div>
+          <h1>Ds movie</h1>
+          <a href="https://github.com/Viviansol"></a>
+        </div>
+
+      </nav>
+    </header>
   );
 }
 
